dc9cd63c814d577616f60dd354c15ad97fbec539
Kayk magalhaes <kaykapple1@gmail.com>
2026-08-30 15:09:34 -0300

    feat:TestConfig

--- src/main/java/com/projetoSpring/project/entities/Category.java
+++ src/main/java/com/projetoSpring/project/entities/Category.java
@@ -1,5 +1,6 @@
 package com.projetoSpring.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
