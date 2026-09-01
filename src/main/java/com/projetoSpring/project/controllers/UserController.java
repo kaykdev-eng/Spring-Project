9647783ce1237a1b1763e0694deb8398ab3be891
Kayk magalhaes <kaykapple1@gmail.com>
2026-08-31 22:19:38 -0300

    insert User

--- src/main/java/com/projetoSpring/project/controllers/UserController.java
+++ src/main/java/com/projetoSpring/project/controllers/UserController.java
@@ -2,10 +2,13 @@ package com.projetoSpring.project.controllers;
 
 import com.projetoSpring.project.entities.User;
 import com.projetoSpring.project.services.UserService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,6 +28,12 @@ public class UserController {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
-    
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 
 }
--- src/main/java/com/projetoSpring/project/services/UserService.java
+++ src/main/java/com/projetoSpring/project/services/UserService.java
@@ -20,4 +20,8 @@ public class UserService {
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
 }
