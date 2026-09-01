568be8066630baff1e92ffeddb2d41ab9eb0bbc7
Kayk magalhaes <kaykapple1@gmail.com>
2026-09-01 20:32:57 -0300

    User update

--- src/main/java/com/projetoSpring/project/services/UserService.java
+++ src/main/java/com/projetoSpring/project/services/UserService.java
@@ -5,6 +5,7 @@ import com.projetoSpring.project.repositories.UserRepository;
 import com.projetoSpring.project.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,15 +18,30 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
+    @Transactional
     public User insert(User obj) {
         return userRepository.save(obj);
     }
 
+    @Transactional
     public void delete(Long id) {
         userRepository.deleteById(id);
+    }
+
+    public User update(Long id, User user) {
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity, user);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
     }
 }
