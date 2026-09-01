1ca2646ae54952db66e4db0bd626200fe0644d86
Kayk magalhaes <kaykapple1@gmail.com>
2026-08-31 21:52:06 -0300

    Subtotal & Total methods

--- src/main/java/com/projetoSpring/project/entities/Order.java
+++ src/main/java/com/projetoSpring/project/entities/Order.java
@@ -29,7 +29,7 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
-    
+
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
@@ -86,6 +86,14 @@ public class Order implements Serializable {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Double getTotal() {
+        Double total = 0.0;
+        for(OrderItem x : items) {
+            total += x.getSubTotal();
+        }
+        return total;
     }
 
     @Override
