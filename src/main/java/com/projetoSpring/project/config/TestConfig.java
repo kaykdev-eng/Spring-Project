7d6bd179091bbff8575f6c9a73dc40bbf1e0ee69
Kayk magalhaes <kaykapple1@gmail.com>
2026-08-31 21:32:34 -0300

    Payment: OnetoOne

--- src/main/java/com/projetoSpring/project/config/TestConfig.java
+++ src/main/java/com/projetoSpring/project/config/TestConfig.java
@@ -68,5 +68,9 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment py1 = new Payment(null, Instant.parse("2026-07-20T19:53:09Z"), o1);
+        o1.setPayment(py1);
+        orderRepository.save(o1);
     }
 }
