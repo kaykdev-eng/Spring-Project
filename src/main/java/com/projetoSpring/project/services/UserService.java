25870109972ce510209480cdb46c94f4ee885ae8
Kayk magalhaes <kaykapple1@gmail.com>
2026-09-01 21:59:39 -0300

    put exception

--- src/main/java/com/projetoSpring/project/services/UserService.java
+++ src/main/java/com/projetoSpring/project/services/UserService.java
@@ -4,6 +4,7 @@ import com.projetoSpring.project.entities.User;
 import com.projetoSpring.project.repositories.UserRepository;
 import com.projetoSpring.project.services.exceptions.DatabaseException;
 import com.projetoSpring.project.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
 
     @Transactional
     public User update(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try{
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
